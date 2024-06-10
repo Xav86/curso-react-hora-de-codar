8506e795e4f0f943f9d447d06e7323cacbff2665
Xav86 <gustavo.g.2005@gmail.com>
2024-06-10 11:44:14 -0300

    Vendo o essencial de JSX

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
 function App() {
+
+  const name = "Gustavo"
+
+  const newName = name.toUpperCase();
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
-      <h1>Olá Mundo!</h1>
-      <p>Meu primeiro app em React</p>
+      <h2>Alterando JSX</h2>
+      <p>Meu nome é: {newName}</p>
+      <p>Soma: {sum(2, 3)}</p>
+      <img src={url} alt="Imagem de 105px por 150px"/>
     </div>
   );
 }
